9627d3b72f1aa589c4a3a9ef968d51973b429c67
Ahmed El Gabri <ahmed@gabri.me>
2024-03-17 23:09:23 +0100

    Handle media when parsing Markdown

--- src/shared.ts
+++ src/shared.ts
@@ -7,17 +7,11 @@ import rehypeRemark from 'rehype-remark'
 import remarkGfm from 'remark-gfm'
 import frontmatter from 'remark-frontmatter'
 import remarkStringify from 'remark-stringify'
-import rehypeSanitize from 'rehype-sanitize'
+import rehypeSanitize, {defaultSchema} from 'rehype-sanitize'
 import {visit} from 'unist-util-visit'
+import {toHtml} from 'hast-util-to-html'
 import mustache from 'mustache'
-import type {
-	Link,
-	Image,
-	ImageReference,
-	LinkReference,
-	Root,
-	Parent,
-} from 'mdast'
+import type {Link, Image, Root, Parent} from 'mdast'
 // @ts-expect-error lack of types
 import layout from './template.html'
 
@@ -137,10 +131,52 @@ export async function convertToMarkdown(
 			2,
 		)}`,
 	)
-	// @TODO: handle media??
 	const md = await processor()
-		.use(rehypeSanitize)
-		.use(rehypeRemark)
+		.use(rehypeSanitize, {
+			// Allow tags/props we want to keep
+			...defaultSchema,
+			attributes: {
+				...defaultSchema.attributes,
+				video: ['src', 'controls'],
+				audio: ['src', 'controls'],
+				iframe: [
+					'src',
+					'width',
+					'height',
+					'title',
+					'frameborder',
+					'allow',
+					'allowfullscreen',
+				],
+			},
+			tagNames: [...(defaultSchema.tagNames || []), 'video', 'audio', 'iframe'],
+		})
+		.use(rehypeRemark, {
+			// Handle tags we want to keep raw, mostly media
+			handlers: {
+				audio(state, node) {
+					if (!node.properties.controls) {
+						node.properties.controls = true
+					}
+					const result = {type: 'html', value: toHtml(node)} as const
+					state.patch(node, result)
+					return result
+				},
+				video(state, node) {
+					if (!node.properties.controls) {
+						node.properties.controls = true
+					}
+					const result = {type: 'html', value: toHtml(node)} as const
+					state.patch(node, result)
+					return result
+				},
+				iframe(state, node) {
+					const result = {type: 'html', value: toHtml(node)} as const
+					state.patch(node, result)
+					return result
+				},
+			},
+		})
 		.use(resolveRelativeURls, {base: fmData.url})
 		.use(remarkGfm)
 		.use(addFrontmatter, fmData)
@@ -156,6 +192,7 @@ export async function convertToMarkdown(
 // title: <title>?
 // author: OG author?
 // excerpt: <meta description>?
+// @FIXME: readability removes `<aside>`, drop it and switch to unified.
 export async function parseHtml(html: string) {
 	console.log(`parsing HTML`)
 	const {document} = parseHTML(html)
@@ -166,7 +203,11 @@ export async function parseHtml(html: string) {
 	const result = reader.parse()
 
 	console.log(
-		`Returning data from Readability ${JSON.stringify(result, null, 2)}`,
+		`Returning data from Readability ${JSON.stringify(
+			{...result, content: 'removed because too big'},
+			null,
+			2,
+		)}`,
 	)
 
 	const content =
@@ -193,9 +234,10 @@ export function buildObsidianURL({
 	fileName: string
 	fileContent: string
 }) {
-	let url = `obsidian://new?file=${encodeURIComponent(
-		`${folder}${fileName}`,
-	)}&content=${encodeURIComponent(fileContent)}`
+	// @NOTE: Encoding the content can lead to issues like this https://stackoverflow.com/questions/25244361/%C3%A2%E2%82%AC-character-showing-instead-of-em-dash
+	let url = `obsidian://new?file=${encodeURIComponent(`${folder}${fileName}`)}`
+
+	url += `&content=${encodeURIComponent(fileContent)}`
 
 	if (vault) {
 		url += `&vault=${vault}`
